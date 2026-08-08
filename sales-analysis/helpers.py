dae1b5e88d3b08c0d9c5b632b00df09d0f0de31a
ulasbedukk <ulasbedukk@gmail.com>
2026-08-08 12:15:50 +0100

    Remove unnecessary files from GitHub tracking

--- sales-analysis/helpers.py
+++
@@ -1,9 +0,0 @@
-# helpers.py
-
-def calculate_total(quantity, price):
-    """Calculate total for a single item"""
-    return quantity * price
-
-def format_currency(amount):
-    """Format number as currency"""
-    return f"${amount:,.2f}"
